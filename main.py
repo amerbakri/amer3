ec745841276c6e76104b149866989cb3ebd41243
amerbakri <94579195+amerbakri@users.noreply.github.com>
2025-08-02 18:21:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import os
-from flask import Flask, request
 import asyncio
+from flask import Flask, request
 from telegram import Bot, Update
-from telegram.ext import Application, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
-    raise RuntimeError("BOT_TOKEN is not set!")
+    raise RuntimeError("BOT_TOKEN is not set")
 
 bot = Bot(token=BOT_TOKEN)
 app = Flask(__name__)
 
 application = Application.builder().token(BOT_TOKEN).build()
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"📥 وصلني أمر /start من: {update.effective_user.id}")
+    await update.message.reply_text("أهلاً بك في البوت!")
+
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"📩 رسالة نصية: {update.message.text} من: {update.effective_user.id}")
     await update.message.reply_text(f"رسالتك: {update.message.text}")
 
+application.add_handler(CommandHandler("start", start))
 application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
 @app.route(f"/{BOT_TOKEN}", methods=["POST"])
@@ -25,5 +31,4 @@ def webhook():
     return "ok"
 
 if __name__ == "__main__":
-    port = 10000
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
